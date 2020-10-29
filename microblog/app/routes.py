49f0d21d8c2d710a0e4ab1f5ccd6076175281eda
Al Harkan <raihankalla@gmail.com>
2020-10-29 21:03:38 +0700

    Chapter 3 & 4: Web Forms & Database

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,12 +8,23 @@ def index():
     user = {'username': 'Al Harkan'}
     posts = [
         {
-            'author': {'username': 'John'},
+            'author': {'username': 'Al'},
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': {'username': 'Susan'},
+            'author': {'username': 'Diana'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+from flask import render_template, flash, redirect
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
