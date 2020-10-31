b278d0d33af6739048d499baba8cbdec81d2436e
Al Harkan <raihankalla@gmail.com>
2020-10-31 22:29:22 +0700

    Chapter 5 - Resolved Git Changes

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
-=======
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
->>>>>>> master
 
 @app.route('/')
 @app.route('/index')
@@ -26,7 +20,6 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-<<<<<<< HEAD
     return render_template("index.html", title='Home Page', posts=posts)
 
 
@@ -65,17 +58,3 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
-=======
-    return render_template('index.html', title='Home', user=user, posts=posts)
-
-from flask import render_template, flash, redirect
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
->>>>>>> master
