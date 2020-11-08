5514537870d4e2dc72a97518924fbe03a70c4a47
Al Harkan <raihankalla@gmail.com>
2020-11-08 13:17:42 +0700

    Chapter 13 - Localization (Language & Datetime)

--- microblog/config.py
+++ microblog/config.py
@@ -13,4 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['raihankalla@gmail.com']
+    LANGUAGES = ['en', 'es', 'id']
     POSTS_PER_PAGE = 25
